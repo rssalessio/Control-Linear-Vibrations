03dee64d9c6128e3262d367987c18d043744127b
rssalessio <russo.alessio890@gmail.com>
2016-03-12 09:22:49 +0100

    Updated folder with last changes/cleaned up

--- funzioniGeneriche/fourierSignal.m
+++ funzioniGeneriche/fourierSignal.m
@@ -11,7 +11,7 @@ function [y] = fourierSignal(mod, phase, f, t)
     y = zeros(length(t),1);
     for i = 1:length(mod)
         omega = 2*pi*f(i);
-        y= y+mod(i)*cos(omega*t+phase(i)); 
+        y= y+mod(i)*sin(omega*t+pi/2+phase(i)); 
     end
 
 end
