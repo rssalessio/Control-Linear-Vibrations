14d7b56ccdb70cc45b909049929f8a81693e718e
gsavaia <gianluca.savaia@gmail.com>
2016-03-23 08:54:32 +0100

    update plots and reads

--- src/Functions/plots.m
+++ src/Functions/plots.m
@@ -1,18 +1,31 @@
-function [t, s1,s2,s3,s4,s5,s6] = plots(file, gr, n)
-
-    if nargin == 1
+function [t, s1,s2,s3,s4,s5,s6] = plots(gr, n)
+    
+    if nargin == 0
         n = 3;
         gr = 0;
     end
     
-    if nargin == 2
+    if nargin == 1
         n = 3;
     end
     
-    [s1, s2, s3, s4, s5, s6] = textread(file, '%f %f %f %f %f %f');
+    [s1, s2, s3, s4, s5, s6] = textread('data0000.txt', '%f %f %f %f %f %f');
+    
+    len = length(s1)-1;
+    
+    if exist('data0001.txt', 'file')
+        [s11, s22, s33, s44, s55, s66] = textread('data0001.txt', '%f %f %f %f %f %f');
+        s1 = [s1;s11];
+        s2 = [s2;s22];
+        s3 = [s3;s33];
+        s4 = [s4;s44];
+        s5 = [s5;s55];
+        s6 = [s6;s66];
+        len = length(s1)-1;
+    end
     
     ts = 1/200; %sampling frequency 100Hz
-    t = 0:ts:(length(s1)-1)*ts;
+    t = 0:ts:len*ts;
     
     figure;
     subplot 311; plot(t,s1); if gr grid; end
--- src/Functions/reads.m
+++ src/Functions/reads.m
@@ -1,8 +1,21 @@
-function [t, s1,s2,s3,s4,s5,s6] = reads(file)
+function [t, s1,s2,s3,s4,s5,s6] = reads()
     
-    [s1, s2, s3, s4, s5, s6] = textread(file, '%f %f %f %f %f %f');
+    [s1, s2, s3, s4, s5, s6] = textread('data0000.txt', '%f %f %f %f %f %f');
+    
+    len = length(s1)-1;
+    
+    if exist('data0001.txt', 'file')
+        [s11, s22, s33, s44, s55, s66] = textread('data0001.txt', '%f %f %f %f %f %f');
+        s1 = [s1;s11];
+        s2 = [s2;s22];
+        s3 = [s3;s33];
+        s4 = [s4;s44];
+        s5 = [s5;s55];
+        s6 = [s6;s66];
+        len = length(s1)-1;
+    end
     
     ts = 1/200; %sampling frequency 100Hz
-    t = 0:ts:(length(s1)-1)*ts;
+    t = 0:ts:len*ts;
     
 end
