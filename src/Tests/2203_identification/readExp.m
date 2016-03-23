f83418e6297c16b11ce384fd96b434813aaab755
rssalessio <russo.alessio890@gmail.com>
2016-03-23 15:59:55 +0100

    Update readexperiment

--- src/Tests/2203_identification/readExp.m
+++ src/Tests/2203_identification/readExp.m
@@ -1,4 +1,4 @@
-function [t,v,i,x] = readExp()
+function [t,v,i,x,xv] = readExp()
 
 [t,v,i,x]=reads();
 
@@ -6,9 +6,10 @@ i = lowPassFilter(i,30);
 
 ts = (find(i>0.2,1)-2)*1/200;
 v(t<ts)=0;
-plot(t,v); hold on; plot(t,i); grid;
 
 x = x./max(abs(x));
 
 %%you have to set v to 0 by yourself
+v(28*200:end)=0;
+xv = cartSpeedFromEncoder(x);
 end
