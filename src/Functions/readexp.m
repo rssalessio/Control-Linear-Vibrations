35a648fb144b374711318a080b085994f8a7c6ae
gsavaia <gianluca.savaia@gmail.com>
2016-03-24 21:36:01 +0100

    utilities for data exctraction

---
+++ src/Functions/readexp.m
@@ -0,0 +1,24 @@
+function [data] = readexp()
+
+[t,v,i,x]=reads();
+
+i = lowPassFilter(i,30,4,0,1/200);
+
+ts = (find(i>0.2,1)-2)*1/200;
+v(t<ts)=0;
+
+x = x./max(abs(x));
+
+%%you have to set v to 0 by yourself
+v(28*200:end)=0;
+xv = cartSpeedFromEncoder(x);
+
+data = struct;
+
+data.t = t;
+data.v = v;
+data.i = i;
+data.x = x;
+data.xv = xv;
+
+end
