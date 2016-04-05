5aee2cae5fdb6141476a81fe5c23fe1e63ada191
Alessio Russo <russo.alessio890@gmail.com>
2016-04-05 11:48:24 +0200

    Clean up of useless code

--- src/Tests/1503_identification/alessio/EncoderTest_ert_rtw/hooks.d
+++
@@ -1,2 +0,0 @@
-hooks.o: \
- C:/MATLAB/SupportPackages/R2015b/arduino-1.6.1/hardware/arduino/sam/cores/arduino/hooks.c
--- src/Tests/1503_identification/alessio/EncoderTest_ert_rtw/iar_calls_sam3.d
+++
@@ -1,2 +0,0 @@
-iar_calls_sam3.o: \
- C:/MATLAB/SupportPackages/R2015b/arduino-1.6.1/hardware/arduino/sam/cores/arduino/iar_calls_sam3.c
