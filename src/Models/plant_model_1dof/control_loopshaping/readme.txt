c62eb649c4c3e196b6def82e3aacd4534dd1cef2
gsavaia <gianluca.savaia@gmail.com>
2016-04-13 19:49:15 +0200

    loopshaping control design

---
+++ src/Models/plant_model_1dof/control_loopshaping/readme.txt
@@ -0,0 +1,8 @@
+1) minreg
+cancellazione poli complessi, banda 2Hz, rising < 1sec
+la corrente non dovrebbe saturare
+
+2) zeroreg
+come sopra ma uno zero in bassa frequenza per alzare la fase
+ma in realtà è una cazzata perchè attiro un polo in bassa
+e il sistema rallenta tutto
--- src/Models/plant_model_1dof/control_loopshaping_currentloop/param.m
+++ src/Models/plant_model_1dof/control_loopshaping_currentloop/param.m
@@ -28,10 +28,10 @@ close all;
 motor = tf(1, [L R]);
 cart = tf(Ke,[M,Ch,Kh]);
 
-controller_c = 1;
+gain_c = 1;
 filter = tf(1,[1/20 1]);
 
-motor_ol = controller_c*motor*filter;
+motor_ol = gain_c*motor*filter;
 motor_cl = feedback(motor_ol, 1);
 
 figure; margin(motor_ol); grid;
@@ -44,14 +44,18 @@ plant = motor_cl*cart;
 zero1 = -5+22i;
 zero2 = -5-22i;
 pole1 = -100;
-gain = -0.01;
-controller_p = zpk([zero1,zero2], [0, pole1], gain)
+gain_p = -0.03; %rising time = 1 sec
+controller_p = zpk([zero1,zero2], [0, pole1], gain_p)
 
 plant_ol = controller_p*plant;
 plant_cl = feedback(plant_ol,1);
 
-figure; rlocus(plant_ol);
+figure; pzmap(plant_ol); grid;
+poles_cl = pole(plant_cl)
+figure; pzmap(plant_cl); grid;
+
 figure;     margin(plant); grid;
 hold on;    margin(plant_ol);  legend('real','shaped');
-poles_cl = pole(plant_cl)
-figure; rlocus(plant_cl);
+
+
+
