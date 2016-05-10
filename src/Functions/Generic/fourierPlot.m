d8c39f8998122d0907acbf79ab626f9dbf8aba79
albyone92 <albyone92@gmail.com>
2016-05-10 18:08:05 +0200

    0510 test updates

--- src/Functions/Generic/fourierPlot.m
+++ src/Functions/Generic/fourierPlot.m
@@ -2,9 +2,9 @@ function [] = fourierPlot(x,Ts)
     [m,p,f] = fourierCoefficients(x,Ts);
     figure;
     subplot(211);
-    plot(f,m); grid; xlabel('Hz'); title('Module');
+    bar(f,m); grid; xlabel('Hz'); title('Module');
     subplot(212);
-    plot(f,p); grid; xlabel('Hz');title('Phase');
+    bar(f,p); grid; xlabel('Hz');title('Phase');
     
     %figure;
     %semilogx(f, 10*log10(m));grid;
--- src/Tests/Controls/1dof_2masses_kh/alessio/LQG/script.m
+++ src/Tests/Controls/1dof_2masses_kh/alessio/LQG/script.m
@@ -32,21 +32,35 @@ A= [-R/L,0,0;
     Gamma/M, -K/M, -C/M];
 B = [1/L; 0; 0];
 Cy =[ 0,1,0];
-D=0;
+D=0;        
 sys=ss(A,B,Cy,D);
+
+syscd = c2d(sys,1/200);
+
 Rn=1e-5;
 Qn = 1e-5* eye(3);
 %QXU = blkdiag(diag([1,1000,1]),eye(1));
 %QWV = blkdiag(Qn,Rn);
 % klqg = lqg(sys,QXU,QWV,1);
 
-klq = lqr(sys, eye(3), 1, zeros(3,1));
+klq = lqr(sys,50*eye(3), 1, zeros(3,1));
 
 gain = Cy*inv( -(A-B*klq))*B;
 
+
+% An= [-R/L,0,0,0;
+%     0,0,1,0;
+%     Gamma/M, -K/M, -C/M,0;
+%     0,-1,0 ,0];
+% Bn = [B;0];
+% Cn = [Cy,0; 0,0,0,1];
+klqi = lqi(sys, 0.1*eye(4),1,zeros(4,1));
+
+
+
 %%
-sim('model');
-figure;
+sim('modelIntegrator');
+figure; 
 plot(x); 
 grid;
 legend('Position');
