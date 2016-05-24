3c569a0d65fcd45089b488f110b1e920de04c703
rssalessio <russo.alessio890@gmail.com>
2016-05-24 02:50:35 +0200

    update 2 on robust control

--- src/Tests/Controls/1dof_2masses_kh/alessio/RobustControl/robust_script.m
+++ src/Tests/Controls/1dof_2masses_kh/alessio/RobustControl/robust_script.m
@@ -1,4 +1,4 @@
-G=sysBuilder(1,'l');
+G=sysBuilder(2,'l');
 
 C =  ltiblock.pid('C','pid');
 AP = AnalysisPoint('dLoad');
@@ -7,7 +7,7 @@ CL0.InputName = 'r';
 CL0.OutputName = 'x';
 
 
-R1 = TuningGoal.Tracking('r','x',0.5);
+R1 = TuningGoal.Tracking('r','x',1);
 R2 = TuningGoal.ControllerPoles('C',0,0,100);
 R3 = TuningGoal.Overshoot('r','x', 5);
 R4 = TuningGoal.Rejection('x',10);
@@ -21,8 +21,16 @@ showTunable(CL)
 figure, step(usample(CL,30),getNominal(CL),3)
 title('Load disturbance rejection')
 legend('Sampled uncertainty','Nominal')
-
-Kp=-0.16;
-Ki=-5.37;
-Kd = -0.0346;
+%%
+Kp=-0.167;
+Ki=-5.38;
+Kd = -0.0468;
 Tf = 0.00958;
+
+s=tf('s');
+C=Kp+Ki/s+Kd*s/(Tf*s+1);
+
+figure;
+bode(C*sysBuilder(2,'l')); hold on;
+bode(C*sysBuilder(2,'m')); hold on;
+bode(C*sysBuilder(2,'h')); hold on;
