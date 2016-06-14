50294c1ae613156872cacf3636d649c49fc88b4f
albyone92 <albyone92@gmail.com>
2016-06-14 18:24:55 +0200

    Hinf 3dof script

--- src/Tests/Controls/3dof/hinf+kfcurrent+kfx3/controller_parameters.m
+++ src/Tests/Controls/3dof/hinf+kfcurrent+kfx3/controller_parameters.m
@@ -1,20 +1,11 @@
 clear all, close all
 
-%load kalman;
+load kalman;
 
-[plant, dof] = sysBuilder([2,2,2], ['m', 'h','l'], {'i','x1', 'x2'}, 0);
+[plant, dof] = sysBuilder([2,2,2], ['m', 'h','l'], {'i','x3'}, 0);
 plant = ss(plant);
 
 damp(plant)/2/pi
-
-%% kalman part
-sys=plant;
-sys=sys.NominalValue;
-sys.c=[1/L,0,0,1,0,0,0;0,0,0,0,0,0,0;0,0,0,0,0,0,0];
-systemc2d = (c2d(sys,1/200));
-sysd = ss(c2d(sys,1/200));
-
-sysd2= ss(sysd.a,[sysd.b, sysd.b],sysd.c,sysd.d,1/200,'inputname',{'u'},'outputname',{'y'});
 
 %% Design Controller
 plant_design = plant(2,1);
@@ -31,24 +22,24 @@ figure; margin(series(Hinf, plant_design)); grid;
 figure; bodemag(feedback(series(Hinf,plant_design), 1)); grid; title('T');
 figure; step(feedback(series(Hinf,plant_design), 1))
 
-% %% Integrator
-% Hinf_zpk = zpk(Hinf);
-% [min,ind] = min(Hinf_zpk.p{1});
-% Hinf_zpk.p{1} = [Hinf_zpk.p{1}(1:ind-1); 0; Hinf_zpk.p{1}(ind+1:8)];
-% 
-% figure; hold on;
-% bode(Hinf); bode(Hinf_zpk); legend('Hinf', 'Hinf_integrator');
-% 
-% Hinf = ss(Hinf_zpk);
-% 
-% %% Reduce
-% Hinf_red = reduce(Hinf, 5);
-% 
-% figure; hold on;
-% bode(Hinf); bode(Hinf_red); legend('Hinf', 'Hinf reduced');
-% 
-% Hinf = ss(Hinf_red);
-% figure; step(feedback(series(Hinf,plant_design), 1))
-% 
-% %% Stabilization Hinf
-% figure; rlocus(Hinf);
+%% Integrator
+Hinf_zpk = zpk(Hinf);
+[min,ind] = min(Hinf_zpk.p{1});
+Hinf_zpk.p{1} = [Hinf_zpk.p{1}(1:ind-1); 0; Hinf_zpk.p{1}(ind+1:8)];
+
+figure; hold on;
+bode(Hinf); bode(Hinf_zpk); legend('Hinf', 'Hinf_integrator');
+
+Hinf = ss(Hinf_zpk);
+
+%% Reduce
+Hinf_red = reduce(Hinf, 5);
+
+figure; hold on;
+bode(Hinf); bode(Hinf_red); legend('Hinf', 'Hinf reduced');
+
+Hinf = ss(Hinf_red);
+figure; step(feedback(series(Hinf,plant_design), 1))
+
+%% Stabilization Hinf
+figure; rlocus(Hinf);
--- src/Tests/Controls/3dof/hinf+kfcurrent+kfx3/kalmanTest.m
+++ src/Tests/Controls/3dof/hinf+kfcurrent+kfx3/kalmanTest.m
@@ -1,51 +1,53 @@
+close all
+clear 
 L=0.0024;
 
 
-sys = sysBuilder([0,2,1],['h','l','m']);
+sys = sysBuilder([2,2,2],['m','h','l'],{'i','x1','x2'},0);
 sys=sys.NominalValue;
-%sys.c=[1/L,0,0;0,1,0];
-sys.c=[0,1,0,0,0,0,0;0,0,0,0,0,0,0;0,0,0,0,0,0,0];
+%sys.c=[1/L,0,0,0,0,0,0;0,0,0,1,0,0,0];
 systemc2d=  (c2d(sys,1/200));
 sysd = ss(c2d(sys,1/200));
 
-sysd2= ss(sysd.a,[sysd.b, sysd.b],sysd.c,sysd.d,1/200,'inputname',{'u'},'outputname',{'y'});
+sysd2= ss(sysd.a,sysd.b,sysd.c,sysd.d,1/200,'inputname',{'u'},'outputname',{'y'});
 
 
 Q=0.00001;
-R=0.001;
+R=diag([0.00127,10e-9,10e-9]);
 
 [kalmf,L,P,M,Z]=kalman(sysd2,Q,R);
-kalmf=kalmf(1,:)
+Obs = [kalmf(4,:), kalmf(7,:)];
+save ('kalman','Obs');
 
-%[kalman_dis,L,P,M]=kalmd(sys,Q,R,1/200);
-%kalman_dis=kalman_dis(1,:);
-
-
-t = [0:1/200:100]';
-
-n = length(t);
-
-rng default
-w = sqrt(Q)*randn(n,1);
-v = sqrt(R)*randn(n,1);
+% [kalman_dis,L,P,M]=kalmd(sys,Q,R,1/200);
+% kalman_dis=kalman_dis(1,:);
 
 
-u = sin(t/5);
-yorig = lsim(sysd,u,t);
-
-figure;
-plot(t,yorig,'green');
-
-
-y= lsim(sysd2,[u w],t)+v;
-
-%hold on; plot(t,y,'blue');
-
-
-y=lsim(kalmf, [u, y],t);
-hold on;
-plot(t,y,'red');
-
-figure;
-plot(t,abs(yorig-y));
-
+% t = [0:1/200:100]';
+% 
+% n = length(t);
+% 
+% rng default
+% w = sqrt(Q)*randn(n,1);
+% v = sqrt(R)*randn(n,1);
+% 
+% 
+% u = sin(t/5);
+% yorig = lsim(sysd,u,t);
+% 
+% figure;
+% plot(t,yorig,'green');
+% 
+% 
+% y= lsim(sysd2,[u w],t)+v;
+% 
+% %hold on; plot(t,y,'blue');
+% 
+% 
+% y=lsim(kalmf, [u, y],t);
+% hold on;
+% plot(t,y,'red');
+% 
+% figure;
+% plot(t,abs(yorig-y));
+% 
