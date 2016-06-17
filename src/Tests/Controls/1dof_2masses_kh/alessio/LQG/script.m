453178d7119e27591aa800ab1e6d177767524187
rssalessio <russo.alessio890@gmail.com>
2016-06-17 21:53:59 +0200

    Add lqg 1dof to the report

--- src/Tests/Controls/1dof_2masses_kh/alessio/LQG/script.m
+++ src/Tests/Controls/1dof_2masses_kh/alessio/LQG/script.m
@@ -2,35 +2,27 @@ clear
 close all;
 
 
-sys = sysBuilder(2, 'h',{'x1'});
+sys = sysBuilder(2, 'h',{'i','x1','v1'},0);
 sys=sys.NominalValue;
 
-
-
-
-
-
-Rn=1e-5;
-Qn = 1e-5* eye(3);
 tsim=30;
-syscd = c2d(sys,1/200);
-klq = lqrd(syscd.a,syscd.b,1*diag([1,1,1]), 1,1/200);
+klq = lqr(sys.a,sys.b,1*diag([0,10000,0]), 1);
 
  A=  sys.a;
  B = sys.b;
  Cy = sys.c;
  D=sys.d;       
-gain = 1;%Cy*inv( -(A-B*klq))*B;
+gain = Cy*inv( -(A-B*klq))*B;
 %gain = dcgain(sys);
+%gain=1;
 
 
-
-
+gain=gain(2)
 
 
 %%
 sim('modelLQR',tsim);
 figure; 
-plot(x); 
+plot(x.Time, x.Data(:,2)); 
 grid;
 legend('Position');
