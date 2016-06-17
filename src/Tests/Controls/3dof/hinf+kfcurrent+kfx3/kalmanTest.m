67bca933a60988e956233189c566ba3f6ed9799a
albyone92 <albyone92@gmail.com>
2016-06-17 10:22:11 +0200

    Test ekf 17/6

--- src/Tests/Controls/3dof/hinf+kfcurrent+kfx3/kalmanTest.m
+++ src/Tests/Controls/3dof/hinf+kfcurrent+kfx3/kalmanTest.m
@@ -9,15 +9,15 @@ sys=sys.NominalValue;
 systemc2d=  (c2d(sys,1/200));
 sysd = ss(c2d(sys,1/200));
 
-sysd2= ss(sysd.a,sysd.b,sysd.c,sysd.d,1/200,'inputname',{'u'},'outputname',{'y'});
+% sysd2= ss(sysd.a,sysd.b,sysd.c,sysd.d,1/200,'inputname',{'u'},'outputname',{'y'});
 
 
 Q=0.00001;
 R=diag([0.00127,10e-9,10e-9]);
 
-[kalmf,L,P,M,Z]=kalman(sysd2,Q,R);
-Obs = [kalmf(4,:), kalmf(7,:)];
-save ('kalman','Obs');
+[kalmf,L,P,M,Z]=kalman(sysd,Q,R);
+% Obs = [kalmf(4,:), kalmf(7,:)];
+save ('kalman','kalmf');
 
 % [kalman_dis,L,P,M]=kalmd(sys,Q,R,1/200);
 % kalman_dis=kalman_dis(1,:);
